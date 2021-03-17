14e259a5a16abd514fe0e9fa676c9908f87eed82
AnnaPedych <apedych@gmail.com>
2021-03-17 22:44:57 +0200

    fixed 1964 allure TC (Unsuccessful Login)

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.matchText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -44,7 +45,7 @@ public class LoginTests extends TestBase {
             $(".signInButton button").click();
         });
         step("Verify unsuccessful authorization", () -> {
-            $("[role=alert] span").shouldHave(text("Please enter your password"));
+            $("span[aria-live=polite]").shouldHave(matchText("Please enter your password"));
         });
     }
 }
