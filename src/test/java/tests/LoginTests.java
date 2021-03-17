87db6f03dbbcfefb9cbe1a5e8fccae6bd25facfa
AnnaPedych <apedych@gmail.com>
2021-03-17 22:24:10 +0200

    added 1964 allure TC (Unsuccessful Login)

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -17,7 +18,7 @@ import static io.qameta.allure.Allure.step;
 public class LoginTests extends TestBase {
     @Test
     @DisplayName("Successful Login as Pay As You Go Customer")
-    void loginPayAsYouGoTest() {
+    void successfulLoginPayAsYouGoTest() {
         step("Open main page", () -> open(""));
         step("Sign in as Pay As You Go Customer", () -> {
             $(".rcl-navbar a[title='Sign in']").click();
@@ -28,6 +29,22 @@ public class LoginTests extends TestBase {
         step("Verify successful authorization", () -> {
             $(".top_intro_section h1").shouldHave(text("Welcome,"));
             $(".link_intro_section span").shouldHave(text("Where can I find my account number?"));
+        });
+    }
+
+    @Test
+    @AllureId("1964")
+    @DisplayName("Unsuccessful Login as Pay As You Go Customer (empty psw)")
+    void failedLoginPayAsYouGoTest() {
+        step("Open main page", () -> open(""));
+        step("Sign in as Pay As You Go Customer", () -> {
+            $(".rcl-navbar a[title='Sign in']").click();
+            $("#username").setValue(getTestUsername());
+            $("#password").setValue("");
+            $(".signInButton button").click();
+        });
+        step("Verify unsuccessful authorization", () -> {
+            $("[role=alert] span").shouldHave(text("Please enter your password"));
         });
     }
 }
